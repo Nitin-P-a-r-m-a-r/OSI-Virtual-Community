79b98664fa1c82b97d60ef5431aaedd34b3b3506
Nitin-P-a-r-m-a-r <parmarnitin8182@gmail.com>
2025-07-07 13:55:20 +0530

    day 3 complete code push

---
+++ Day_3/Mission.Api/Mission.Api/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using Mission.Entities;
+using Mission.Repositories.IRepository;
+using Mission.Repositories.Repository;
+using Mission.Services.IServise;
+using Mission.Services.Servise;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<MissionDbContext>(u => u.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Day_3/Mission.Api/Mission.Services/Servise/UserService.cs
@@ -0,0 +1,43 @@
+﻿using Mission.Entities.ViewModels;
+using Mission.Entities.ViewModels.Login;
+using Mission.Repositories.IRepository;
+using Mission.Services.IServise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Services.Servise
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<ResponseResult> LoginUser(UserLoginRequestModel model)
+        {
+            var (response, message) = await _userRepository.LogiUser(model);
+
+            var result = new ResponseResult()
+            {
+                Data = response,
+                Message = message
+            };
+
+            if (response == null)
+            {
+                result.Result = ResponseStatus.Error;
+            }
+            else
+            {
+                result.Result = ResponseStatus.Success;
+            }
+            return result;
+        }
+    }
+}
