fba02bef53b919435d12b0689899978f0de9f618
Nitin-P-a-r-m-a-r <parmarnitin8182@gmail.com>
2025-07-07 22:31:44 +0530

    this is 4th day

---
+++ Day_4/dotnet code/Mission.Services/IServise/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using Mission.Entities.ViewModels;
+using Mission.Entities.ViewModels.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Services.IServise
+{
+    public interface IUserService
+    {
+        // Fixed spelling error in method name and removed 'async' modifier as interface methods cannot have a body.
+        Task<ResponseResult> LoginUser(UserLoginRequestModel model);
+    }
+}
---
+++ Day_4/dotnet code/Mission.Services/Servise/UserService.cs
@@ -0,0 +1,41 @@
+﻿using Mission.Entities.ViewModels;
+using Mission.Entities.ViewModels.Login;
+using Mission.Repositories.IRepository;
+using Mission.Services.IServise;
+using Mission.Services.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Services.Servise
+{
+    public class UserService(IUserRepository userRepository, JwtService jwtService) : IUserService
+    {
+        private readonly IUserRepository _userRepository = userRepository;
+
+        private readonly JwtService _jwtService = jwtService;
+
+        public async Task<ResponseResult> LoginUser(UserLoginRequestModel model)
+        {
+            var (response, message) = await _userRepository.LogiUser(model);
+
+            var result = new ResponseResult()
+            {
+                Message = message
+            };
+
+            if (response == null)
+            {
+                result.Result = ResponseStatus.Error;
+            }
+            else
+            {
+                result.Data = _jwtService.GenerateJwtToken(response);
+                result.Result = ResponseStatus.Success;
+            }
+            return result;
+        }
+    }
+}
