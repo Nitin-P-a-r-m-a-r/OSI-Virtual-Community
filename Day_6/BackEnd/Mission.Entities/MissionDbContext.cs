385bfdeffb647904e8944cfb88f37201e97648d7
Nitin-P-a-r-m-a-r <parmarnitin8182@gmail.com>
2025-07-10 01:00:47 +0530

    Backend Skill and Theme Done, Frontend code updated

---
+++ Day_6/BackEnd/Mission.Entities/MissionDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mission.Entities.Models;
+using System.Collections.Generic;
+
+namespace Mission.Entities
+{
+    public class MissionDbContext(DbContextOptions<MissionDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<MissionSkill> MissionSkills { get; set; }
+        public DbSet<MissionTheme> MissionThemes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegancyTimestampBehavior", true);
+
+            modelBuilder.Entity<User>().HasData(new User()
+            {
+                Id = 1,
+                FirstName = "nitin",
+                LastName = "parmar",
+                EmailAddress = "nitinparmar@gmail.com",
+                Password = "nitin",
+                PhoneNumber = "01234567890",
+                UserType = "admin",
+            });
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
---
+++ Day_6/BackEnd/Mission.Services/IServise/IMissionSkillService.cs
@@ -0,0 +1,19 @@
+﻿using Mission.Entities.ViewModels.MissionSkill;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Services.IServie
+{
+    public interface IMissionSkillService
+    {
+        Task AddMissionSkillAsync(UpsertMissionSkillRequestModel model);
+
+        Task<List<MissionSkillResponseModel>> GetMissionSkillListAsync();
+        Task<MissionSkillResponseModel?> GetMissionSkillByIdAsync(int missionSkillId);
+        Task<bool> UpdateMissionSkillAsync(UpsertMissionSkillRequestModel model);
+        Task<bool> DeleteMissionSkill(int id);
+    }
+}
