22bb1780dceb0767e3d178be3c76b99ca42a978f
Nitin-P-a-r-m-a-r <parmarnitin8182@gmail.com>
2025-07-14 21:43:05 +0530

    Admin dashboard edit profile

---
+++ Day_9/backEnd/Mission.Repositories/IRepository/IMissionSkillRepository.cs
@@ -0,0 +1,18 @@
+﻿using Mission.Entities.ViewModels.MissionSkill;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Services.IServise
+{
+    public interface IMissionSkillRepository
+    {
+        Task AddMissionSkillAsync(UpsertMissionSkillRequestModel model);
+        Task<List<MissionSkillResponseModel>> GetMissionSkillListAsync();
+        Task<MissionSkillResponseModel?> GetMissionSkillByIdAsync(int missionSkillId);
+        Task<bool> UpdateMissionSkillAsync(UpsertMissionSkillRequestModel model);
+        Task<bool> DeleteMissionSkill(int id);
+    }
+}
